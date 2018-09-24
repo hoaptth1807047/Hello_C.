1a17920aca018dda72af2012034894d9615a76ce
Pham Thanh Hoa <hoaptth1807047@fpt.edu.vn>
2018-09-24 19:01:48 +0700

    String handle

---
+++ cmake-build-debug/CMakeFiles/untitled9.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled9.dir/main.c.o"
+  "untitled9.pdb"
+  "untitled9.exe"
+  "libuntitled9.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
